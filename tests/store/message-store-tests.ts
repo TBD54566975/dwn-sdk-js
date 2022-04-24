ee4e5970e6c7809837e607606505afe24ad8d8fa
Moe Jangda <moe@squareup.com>
2022-04-24 09:52:56 -0500

    (message-store) add test for `put`

--- tests/store/message-store-tests.ts
+++ tests/store/message-store-tests.ts
@@ -1,6 +1,16 @@
 import { expect } from 'chai';
 import { MessageStoreLevel } from '../../src/store/message-store';
 
+import { sha256 } from 'multiformats/hashes/sha2';
+
+import * as cbor from '@ipld/dag-cbor';
+import * as Block from 'multiformats/block';
+
+const messageStore = new MessageStoreLevel({
+  blockstoreLocation : 'TEST-BLOCKSTORE',
+  indexLocation      : 'TEST-INDEX'
+});
+
 describe('MessageStoreLevel Tests', () => {
   describe('_buildIndexQueryTerms', () => {
     it('returns an array of terms based on the query object provided', () => {
@@ -40,4 +50,55 @@ describe('MessageStoreLevel Tests', () => {
       expect(terms).to.eql(expected);
     });
   });
+
+  describe('put', () => {
+    before(async () => {
+      await messageStore.open();
+    });
+
+    afterEach(async () => {
+      await messageStore.clear();
+    });
+
+    after(async () => {
+      await messageStore.close();
+    });
+
+    it('stores messages as cbor/sha256 encoded blocks with CID as key', async () => {
+      const msg = {
+        'descriptor': {
+          'ability': {
+            'description' : 'some description',
+            'method'      : 'CollectionsWrite',
+            'schema'      : 'https://schema.org/MusicPlaylist'
+          },
+          'method'    : 'PermissionsRequest' as const,
+          'objectId'  : '03754d75-c6b9-4fdd-891f-7eb2ad4bbd21',
+          'requester' : 'did:jank:alice'
+        },
+        'attestation': {
+          'payload'   : 'farts',
+          'protected' : 'farts',
+          'signature' : 'farts'
+        }
+      };
+
+      await messageStore.put(msg);
+
+      const expectedBlock = await Block.encode({ value: msg, codec: cbor, hasher: sha256 });
+
+      const hasBlock = await messageStore.db.has(expectedBlock.cid);
+      expect(hasBlock).to.be.true;
+
+      const blockBytes = await messageStore.db.get(expectedBlock.cid);
+      expect(blockBytes).to.eql(expectedBlock.bytes);
+    });
+  });
+
+  describe('get', () => {});
+
+  describe('query', () => {});
+
+  describe('delete', () => {});
+
 });
