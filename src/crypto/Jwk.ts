5ee75bce017d9a9120ce5b24654f075f51ee0414
Henry Tsai <henrytsai@squareup.com>
2022-04-25 12:49:13 -0700

    Removed a couple of unnecessary packages.

--- src/crypto/Jwk.ts
+++ src/crypto/Jwk.ts
@@ -1,6 +1,6 @@
-import { Ed25519KeyPair } from '@transmute/ed25519-key-pair';
+import * as ed25519 from '@noble/ed25519';
+import base64url from 'base64url';
 import JwkEd25519 from './JwkEd25519';
-import randomBytes from 'randombytes';
 
 /**
  * Class containing JWK related operations.
@@ -15,19 +15,25 @@ export default class Jwk {
     publicKeyJwk: JwkEd25519,
     privateKeyJwk: Required<JwkEd25519>
   }> {
-    const keyPair = await Ed25519KeyPair.generate({
-      secureRandom: () => randomBytes(32)
-    });
+    const privateKeyUint8Array = ed25519.utils.randomPrivateKey();
+    const publicKeyUint8Array = await ed25519.getPublicKey(privateKeyUint8Array);
+    const privateKeyBuffer = Buffer.from(privateKeyUint8Array);
+    const publicKeyBuffer = Buffer.from(publicKeyUint8Array);
 
-    const exportedKeyPair = await keyPair.export({
-      type: 'JsonWebKey2020',
-      privateKey: true
-    });
-    
-    const { publicKeyJwk, privateKeyJwk } = exportedKeyPair as any;
-    publicKeyJwk.alg = 'EdDSA';
-    privateKeyJwk.alg = 'EdDSA';
- 
+    const d = base64url.encode(privateKeyBuffer);
+    const x = base64url.encode(publicKeyBuffer);
+
+    const publicKeyJwk: JwkEd25519 = {
+      kty: 'OKP',
+      crv: 'Ed25519',
+      x
+    };
+
+    const privateKeyJwk: Required<JwkEd25519> = {
+      ...publicKeyJwk,
+      d
+    }
+
     return { publicKeyJwk, privateKeyJwk};
   }
 }
