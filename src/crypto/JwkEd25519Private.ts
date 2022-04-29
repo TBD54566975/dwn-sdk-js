49d52f318c13c5774e31ee271a5b7bfde3a81986
Henry Tsai <henrytsai@squareup.com>
2022-04-28 18:15:00 -0700

    Using consistent type keyword over interface.

--- src/crypto/JwkEd25519Private.ts
+++ src/crypto/JwkEd25519Private.ts
@@ -3,6 +3,8 @@ import type JwkEd25519Public from './JwkEd25519Public';
 /**
  * Model for representing a Ed25519 private key in JWK format.
  */
-export default interface JwkEd25519Private extends JwkEd25519Public {
+type JwkEd25519Private = JwkEd25519Public & {
   d: string; // Only used by a private key.
 };
+
+export default JwkEd25519Private;
--- src/crypto/JwkSecp256k1Private.ts
+++ src/crypto/JwkSecp256k1Private.ts
@@ -3,6 +3,8 @@ import type JwkSecp256k1Public from './JwkSecp256k1Public';
 /**
  * Model for representing a SECP256K1 private key in JWK format.
  */
-export default interface JwkSecp256k1Private extends JwkSecp256k1Public {
+type JwkSecp256k1Private = JwkSecp256k1Public & {
   d: string; // Only used by a private key.
 };
+
+export default JwkSecp256k1Private;
