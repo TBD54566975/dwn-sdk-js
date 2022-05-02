9ac4f873e0c33f1429c90e9488460ae7069d49b1
Henry Tsai <henrytsai@squareup.com>
2022-05-02 15:21:49 -0700

    Fixed casing

--- tests/jose/jws.spec.ts
+++ tests/jose/jws.spec.ts
@@ -1,16 +1,15 @@
 import { expect } from 'chai';
 import * as jws from '../../src/jose/jws';
-import * as JwkSecp256k1Private from './vectors/jwk-secp256k1-private.json';
+import * as jwkSecp256k1Private from './vectors/jwk-secp256k1-private.json';
 
 describe('Jws', () => {
   it('should sign and verify SECP256K1 signature using a key vector correctly',  async () => {
-    const privateKeyJwk = JwkSecp256k1Private;
-    const publicKeyJwk = Object.assign({ }, privateKeyJwk); // Clone private key.
+    const publicKeyJwk = Object.assign({ }, jwkSecp256k1Private); // Clone private key.
     delete publicKeyJwk.d; // Remove the private key portion.
 
     const protectedHeader = { anyHeader: 'anyHeaderValue' };
     const payloadBuffer = Buffer.from('anyPayloadValue');
-    const jwsObject = await jws.sign(protectedHeader, payloadBuffer, privateKeyJwk);
+    const jwsObject = await jws.sign(protectedHeader, payloadBuffer, jwkSecp256k1Private);
 
     const verificationResult = await jws.verify(jwsObject, publicKeyJwk);
 
