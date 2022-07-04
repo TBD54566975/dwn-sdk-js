f474d19da9be9e9a1b11ca06b209705bba7a0148
Moe Jangda <moe@squareup.com>
2022-07-04 03:12:26 -0500

    address PR comment

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,17 @@
+/**
+ * a Context object is used to pass request-scoped values and cancellation signals across API boundaries.
+ * Context objects are passed all the way down the call-stack
+ */
 export interface Context {
+  /** the attester DID (if present) of the message being processed */
   attester?: string
+  /** the author DID (if present) of the message being processed */
   author?: string
+  /** used as a means to cancel an async operation. More about `AbortSignal` can be read here:
+   * https://developer.mozilla.org/en-US/docs/Web/API/AbortSignal
+   */
   signal?: AbortSignal
+  /** the recipient DID of the message being processed */
   tenant: string
 };
 
