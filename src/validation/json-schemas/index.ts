b48197606af323c31f6f31f78e941b4ffc03f61e
Moe Jangda <moe@squareup.com>
2022-07-04 03:58:47 -0500

    address PR comment

--- src/validation/json-schemas/index.ts
+++ src/validation/json-schemas/index.ts
@@ -1,14 +1,14 @@
-import defs from './defs.schema.json';
+import Definitions from './definitions.json';
 import GeneralJws from './general-jws.json';
-import PermissionsDefs from './permissions/defs.json';
+import PermissionsDefinitions from './permissions/defs.json';
 import PermissionsRequest from './permissions/permissions-request.json';
 import PermissionsGrant from './permissions/permissions-grant.json';
 import Request from './request.json';
 
 export const schemas = {
-  defs,
+  Definitions,
   GeneralJws,
-  PermissionsDefs,
+  PermissionsDefinitions,
   PermissionsGrant,
   PermissionsRequest,
   Request
