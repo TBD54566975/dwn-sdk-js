dcf4f24c00c1d234aac955a57251e16c3a74f194
Henry Tsai <henrytsai@squareup.com>
2022-07-13 06:37:55 +0700

    updated comments

--- tests/utils/cache.spec.ts
+++ tests/utils/cache.spec.ts
@@ -6,14 +6,14 @@ import { MemoryCache } from '../../src/utils/cache';
 chai.use(chaiAsPromised);
 
 describe('MemoryCache', () => {
-  it('should return `undefined` when key-value pair expires',  async () => {
+  it('should return `undefined` when value expires',  async () => {
     const memoryCache = new MemoryCache(0.01); // 0.01 second = 10 millisecond time-to-live
 
     await memoryCache.set('key', 'aValue');
     let valueInCache = await memoryCache.get('key');
     expect(valueInCache).to.equal('aValue');
 
-    await new Promise(resolve => setTimeout(resolve, 10)); // wait for 10 millisecond to key-value to expire
+    await new Promise(resolve => setTimeout(resolve, 10)); // wait for 10 millisecond for value to expire
     valueInCache = await memoryCache.get('key');
     expect(valueInCache).to.be.undefined;
   });
