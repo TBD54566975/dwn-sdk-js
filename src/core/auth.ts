c5a0a1ff65693843e8ac4953ca6fc85bbf16b157
Henry Tsai <henrytsai@squareup.com>
2022-08-18 16:02:06 -0700

    added DID authentication

--- src/core/auth.ts
+++ src/core/auth.ts
@@ -30,38 +30,55 @@ export async function verifyAuth(
   didResolver: DIDResolver,
   payloadConstraints?: PayloadConstraints
 ): Promise<AuthVerificationResult> {
+  // signature verification is computationally intensive, so we're going to start
+  // by validating the payload.
+  const parsedPayload = await validateSchema(message, payloadConstraints);
+
+  const signers = await authenticate(message.authorization, didResolver);
+
+  await authorize(message, signers);
+
+  return { payload: parsedPayload, signers };
+}
+
+async function authorize(message: BaseMessageSchema, signers: string[]): Promise<void> {
+  // // if requester is the same as the target DID, then we can directly grant access
+  // if (signers[0] === message.target) {
+  //   return;
+  // } else {
+  //   throw new Error('unauthorized message');
+  // }
+}
+
+async function authenticate(jws: GeneralJws, didResolver: DIDResolver): Promise<string[]> {
+  const verifier = new GeneralJwsVerifier(jws);
+  const { signers } = await verifier.verify(didResolver);
+  return signers;
+}
+
+async function validateSchema(
+  message: BaseMessageSchema & Authorization,
+  payloadConstraints?: PayloadConstraints
+): Promise<{ descriptorCid: CID, [key: string]: CID }> {
 
   if (message.authorization.signatures.length !== 1) {
     throw new Error('Expected no more than 1 signature for authorization');
   }
 
-  const verifier = new GeneralJwsVerifier(message.authorization);
-
-  // signature verification is computationally intensive, so we're going to start
-  // by validating the payload.
-
-  const payloadBytes: Uint8Array = verifier.decodePayload();
-  const payloadStr = new TextDecoder().decode(payloadBytes);
-  let payloadJson;
-
-  try {
-    payloadJson = JSON.parse(payloadStr);
-  } catch {
-    throw new Error('auth payload must be a valid JSON object');
-  }
+  const payloadJson = GeneralJwsVerifier.decodeJsonPayload(message.authorization);
 
   if (!isPlainObject(payloadJson)) {
     throw new Error('auth payload must be a valid JSON object');
   }
 
+  // the authorization payload should, at minimum, always contain `descriptorCid` regardless
+  // of whatever else is present.
   const { descriptorCid } = payloadJson;
   if (!descriptorCid) {
     throw new Error('descriptorCid must be present in authorization payload');
   }
 
-  // the authorization payload should, at minimum, always contain `descriptorCid` regardless
-  // of whatever else is present. check to ensure that the provided descriptorCid matches
-  // the CID of the actual message
+  // check to ensure that the provided descriptorCid matches the CID of the actual message
 
   // parseCid throws an exception if parsing fails
   const providedDescriptorCid = parseCid(descriptorCid);
@@ -71,6 +88,7 @@ export async function verifyAuth(
     throw new Error('provided descriptorCid does not match expected CID');
   }
 
+  // property bag for all properties inspected
   const parsedPayload = { descriptorCid: providedDescriptorCid };
 
   payloadConstraints = payloadConstraints || { properties: new Set([]) };
@@ -87,13 +105,11 @@ export async function verifyAuth(
     try {
       parsedPayload[field] = parseCid(payloadJson[field]);
     } catch (e) {
-      throw new Error(`${field} must be a valid CID`);
+      throw new Error(`${field} must be a valid CID`); // TODO: Sanity: why does this must be a CID?
     }
   }
 
-  const { signers } = await verifier.verify(didResolver);
-
-  return { payload: parsedPayload, signers };
+  return parsedPayload;
 }
 
 /**
