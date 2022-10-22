a64881c9564764822a304a7b12b961022d5e3643
Cxxshyy <74113508+Cxxshyy@users.noreply.github.com>
2022-10-23 00:31:38 +0100

    refactored entire codebase to use encoder.ts
    * refactored entire codebase to use encoder.ts
    
    Co-authored-by: Henry Tsai <henrytsai@outlook.com>

--- src/utils/encoder.ts
+++ src/utils/encoder.ts
@@ -1,9 +1,14 @@
 import { base64url } from 'multiformats/bases/base64';
 
 const textEncoder = new TextEncoder();
+const textDecoder = new TextDecoder();
 
 export function stringToBytes(content: string): Uint8Array {
   const bytes = textEncoder.encode(content);
+  return bytes;
+}
+export function bytesToString(content: Uint8Array): string {
+  const bytes = textDecoder.decode(content);
   return bytes;
 }
 
@@ -13,12 +18,13 @@ export function stringToBase64Url(content: string): string {
   return base64UrlString;
 }
 
-export function base64urlToBytes(base64urlString: string): Uint8Array {
-  const content = base64url.baseDecode(base64urlString);
-  return content;
-}
 
 export function bytesToBase64Url(bytes: Uint8Array): string {
   const base64UrlString = base64url.baseEncode(bytes);
   return base64UrlString;
+}
+
+export function base64urlToBytes(base64urlString: string): Uint8Array {
+  const content = base64url.baseDecode(base64urlString);
+  return content;
 }
