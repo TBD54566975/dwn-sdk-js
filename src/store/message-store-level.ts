3a1d8610f8acce23917b967c40addc2dc95afe0d
Nick Wylynko <nick@wylynko.com>
2022-10-27 02:02:36 +0800

    Use typescript utility types to define the index search instance
    * Use typescript utility types to define the index search instance
    * index.QUERY returns RESULT in an object not RESULTS
    * Update @types/search-index to version 3.2.0
    Co-authored-by: Nick Wylynko <nick@blueskylabs.com.au>
    Co-authored-by: Moe Jangda <moe@squareup.com>
    Co-authored-by: Henry Tsai <henrytsai@outlook.com>

--- src/store/message-store-level.ts
+++ src/store/message-store-level.ts
@@ -22,8 +22,8 @@ export class MessageStoreLevel implements MessageStore {
   db: BlockstoreLevel;
   // levelDB doesn't natively provide the querying capabilities needed for DWN. To accommodate, we're leveraging
   // a level-backed inverted index
-  // TODO: search-index lib does not import type `SearchIndex`. find a workaround, Issue #48, https://github.com/TBD54566975/dwn-sdk-js/issues/48
-  index;
+  // search-index lib does not import type `SearchIndex`. get the awaited return type as a workaround
+  index: Awaited<ReturnType<typeof searchIndex>>;
 
   /**
    * @param {MessageStoreLevelConfig} config
