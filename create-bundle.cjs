15c1fcd265566dfa4892aaedc8a00de49faac9f9
Moe Jangda <moe@squareup.com>
2022-10-28 17:33:09 -0500

    bundle node global polyfills

--- create-bundle.cjs
+++ create-bundle.cjs
@@ -1,8 +1,11 @@
+const { NodeGlobalsPolyfillPlugin } = require('@esbuild-plugins/node-globals-polyfill');
+
 require('esbuild').build({
   entryPoints : ['./src/index.ts'],
   bundle      : true,
   minify      : true,
   sourcemap   : true,
   target      : ['chrome101'],
+  plugins     : [NodeGlobalsPolyfillPlugin({ process: true })],
   outfile     : 'dist/bundles/index.cjs',
 });
