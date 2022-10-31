4a26c50675c19bde704040c3abac64e813a9be82
Josh Floth <thadiusbacon@gmail.com>
2022-10-30 20:44:41 -0500

    tests and linting

--- tests/validation/json-schemas/public-jwk.spec.ts
+++ tests/validation/json-schemas/public-jwk.spec.ts
@@ -7,13 +7,13 @@ const { Ed25519, secp256k1 } = signers;
 describe('PublicJwk Schema', async () => {
   const { publicJwk: publicJwkSecp256k1 } = await secp256k1.generateKeyPair();
   const { publicJwk: publicJwkEd25519 } = await Ed25519.generateKeyPair();
-  
+
   const publicJwkRsa = {
-    "kty": "RSA",
-    "e": "AQAB",
-    "use": "sig",
-    "alg": "RS256",
-    "n": "abcd1234"
+    'kty' : 'RSA',
+    'e'   : 'AQAB',
+    'use' : 'sig',
+    'alg' : 'RS256',
+    'n'   : 'abcd1234'
   };
 
   [publicJwkSecp256k1, publicJwkEd25519, publicJwkRsa].forEach((publicJwk): void => {
