0af32e24ad11fe98e6613d4a6864b868b966a1a4
Cxxshyy <djjames.catlyn@gmail.com>
2022-11-12 12:30:41 +0000

    added tests

--- tests/did/did-resolver.spec.ts
+++ tests/did/did-resolver.spec.ts
@@ -3,6 +3,8 @@ import chaiAsPromised from 'chai-as-promised';
 import sinon from 'sinon';
 import { DidIonResolver } from '../../src/did/did-ion-resolver';
 import { DidResolver } from '../../src/did/did-resolver';
+import { MemoryCache } from '../../src/utils/memory-cache';
+import { Cache } from '../../src/utils/types';
 
 // extends chai to test promises
 chai.use(chaiAsPromised);
@@ -22,4 +24,20 @@ describe('DidResolver', () => {
 
     expect(ionDidResolveSpy.called).to.be.true;
   });
+
+  it('should pick the right DID resolve based on DID method name and passed in cache', async () => {
+    const did = 'did:ion:unusedDid';
+    const didIonResolver = new DidIonResolver('unusedResolutionEndpoint');
+    const cache = new MemoryCache(500);
+    const didResolver = new DidResolver([didIonResolver],cache);
+
+    const ionDidResolveSpy = sinon.stub(didIonResolver, 'resolve').resolves({
+      didDocument           : 'unused' as any,
+      didDocumentMetadata   : 'unused' as any,
+      didResolutionMetadata : 'unused' as any
+    });
+    await didResolver.resolve(did);
+
+    expect(ionDidResolveSpy.called).to.be.true;
+  })
 });
