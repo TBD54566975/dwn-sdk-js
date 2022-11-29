9cfc0640d129a83fc5d70fe5e81caf57f9199282
Cxxshyy <74113508+Cxxshyy@users.noreply.github.com>
2022-11-29 05:53:46 +0000

    added cache for did-resolver
    Co-authored-by: Michael Neale <michael.neale@gmail.com>
    Co-authored-by: Henry Tsai <henrytsai@squareup.com>

--- tests/did/did-resolver.spec.ts
+++ tests/did/did-resolver.spec.ts
@@ -8,18 +8,27 @@ import { DidResolver } from '../../src/did/did-resolver';
 chai.use(chaiAsPromised);
 
 describe('DidResolver', () => {
-  it('should pick the right DID resolver based on DID method name', async () => {
+  it('should cache the resolution result and use the cached result when available', async () => {
     const did = 'did:ion:unusedDid';
     const didIonResolver = new DidIonResolver('unusedResolutionEndpoint');
     const didResolver = new DidResolver([didIonResolver]);
 
-    const ionDidResolveSpy = sinon.stub(didIonResolver, 'resolve').resolves({
-      didDocument           : 'unused' as any,
-      didDocumentMetadata   : 'unused' as any,
-      didResolutionMetadata : 'unused' as any
-    });
-    await didResolver.resolve(did);
+    const mockResolution = {
+      didDocument           : 'any' as any,
+      didDocumentMetadata   : 'any' as any,
+      didResolutionMetadata : 'any' as any
+    };
+    const ionDidResolveSpy = sinon.stub(didIonResolver, 'resolve').resolves(mockResolution);
 
-    expect(ionDidResolveSpy.called).to.be.true;
+    const cacheGetSpy = sinon.spy(didResolver['cache'], 'get');
+
+    // calling resolve twice
+    const resolutionResult1 = await didResolver.resolve(did);
+    expect(resolutionResult1).to.equal(mockResolution);
+    const resolutionResult2 = await didResolver.resolve(did);
+    expect(resolutionResult2).to.equal(mockResolution);
+
+    sinon.assert.calledTwice(cacheGetSpy); // should try to fetch from cache both times
+    sinon.assert.calledOnce(ionDidResolveSpy); // should only resolve using ION resolver once (the first time)
   });
 });
