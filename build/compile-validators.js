770a96c338990410d98fee78b124b4d5e0a9af17
Henry Tsai <henrytsai@squareup.com>
2022-12-09 13:32:22 -0800

    removed the need for --es-module-specifier-resolution flag for compile-validators.js

--- build/compile-validators.js
+++ build/compile-validators.js
@@ -14,8 +14,8 @@ import path from 'node:path';
 import url from 'node:url';
 
 import Ajv from 'ajv';
-import standaloneCode from 'ajv/dist/standalone';
 import mkdirp from 'mkdirp';
+import standaloneCode from 'ajv/dist/standalone/index.js';
 
 import CollectionsQuery from '../json-schemas/collections/collections-query.json' assert { type: 'json' };
 import CollectionsWrite from '../json-schemas/collections/collections-write.json' assert { type: 'json' };
@@ -25,8 +25,8 @@ import GeneralJws from '../json-schemas/general-jws.json' assert { type: 'json' 
 import HooksWrite from '../json-schemas/hooks/hooks-write.json' assert { type: 'json' };
 import JwkVerificationMethod from '../json-schemas/jwk-verification-method.json' assert {type: 'json'};
 import PermissionsDefinitions from '../json-schemas/permissions/definitions.json' assert { type: 'json' };
-import PermissionsRequest from '../json-schemas/permissions/permissions-request.json' assert { type: 'json' };
 import PermissionsGrant from '../json-schemas/permissions/permissions-grant.json' assert { type: 'json' };
+import PermissionsRequest from '../json-schemas/permissions/permissions-request.json' assert { type: 'json' };
 import ProtocolDefinition from '../json-schemas/protocol-definition.json' assert { type: 'json' };
 import ProtocolRuleSet from '../json-schemas/protocol-rule-set.json' assert { type: 'json' };
 import ProtocolsConfigure from '../json-schemas/protocols/protocols-configure.json' assert { type: 'json' };
