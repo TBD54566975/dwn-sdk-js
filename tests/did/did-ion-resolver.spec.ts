76f0fafca20422817dbafdaf83550353c5cd52fa
Henry Tsai <henrytsai@outlook.com>
2022-12-13 09:25:51 -0800

    #83 - updated default ION resolver endpoint

--- tests/did/did-ion-resolver.spec.ts
+++ tests/did/did-ion-resolver.spec.ts
@@ -9,14 +9,14 @@ import { DidIonResolver } from '../../src/did/did-ion-resolver.js';
 chai.use(chaiAsPromised);
 
 describe('DidIonResolver', () => {
-  const resolutionEndpoint = 'https://beta.discover.did.microsoft.com/1.0/identifiers/';
+  const defaultResolutionEndpoint = 'https://discover.did.microsoft.com/1.0/identifiers/';
   let networkAvailable = false;
   before(async () => {
     // test network connectivity, `networkAvailable` is used by tests to decide whether to run tests through real network calls or stubs
-    const testDid = 'https://beta.discover.did.microsoft.com/1.0/identifiers/did:ion:EiClkZMDxPKqC9c-umQfTkR8vvZ9JPhl_xLDI9Nfk38w5w';
+    const testDidUrl = `${defaultResolutionEndpoint}did:ion:EiClkZMDxPKqC9c-umQfTkR8vvZ9JPhl_xLDI9Nfk38w5w`;
 
     try {
-      const response = await fetch(testDid);
+      const response = await fetch(testDidUrl);
 
       if (response.status === 200) {
         networkAvailable = true;
@@ -29,12 +29,12 @@ describe('DidIonResolver', () => {
   it('should set a default resolution endpoint when none is given in constructor', async () => {
     const didIonResolver = new DidIonResolver();
 
-    expect(didIonResolver['resolutionEndpoint']).to.equal('https://beta.discover.did.microsoft.com/1.0/identifiers/');
+    expect(didIonResolver['resolutionEndpoint']).to.equal(defaultResolutionEndpoint);
   });
 
   it('should resolve an ION DID correctly', async () => {
     const did = 'did:ion:EiClkZMDxPKqC9c-umQfTkR8vvZ9JPhl_xLDI9Nfk38w5w';
-    const didIonResolver = new DidIonResolver(resolutionEndpoint);
+    const didIonResolver = new DidIonResolver();
 
     // stub network call if network is not available
     if (!networkAvailable) {
@@ -54,7 +54,7 @@ describe('DidIonResolver', () => {
 
   it('should throw if ION DID cannot be resolved', async () => {
     const did = 'did:ion:SomethingThatCannotBeResolved';
-    const didIonResolver = new DidIonResolver(resolutionEndpoint);
+    const didIonResolver = new DidIonResolver();
 
     // stub network call if network is not available
     if (!networkAvailable) {
