cbe52e44956a98d4f24e16050655cf96bc7b2565
Henry Tsai <henrytsai@squareup.com>
2023-01-05 13:28:14 -0800

    removed exception catching when processing messagea

--- src/interfaces/collections/handlers/collections-write.ts
+++ src/interfaces/collections/handlers/collections-write.ts
@@ -12,105 +12,99 @@ export const handleCollectionsWrite: MethodHandler = async (
   messageStore,
   didResolver
 ): Promise<MessageReply> => {
+  const incomingMessage = message as CollectionsWriteMessage;
+
+  let collectionsWrite: CollectionsWrite;
   try {
-    const incomingMessage = message as CollectionsWriteMessage;
-
-    let collectionsWrite: CollectionsWrite;
-    try {
-      collectionsWrite = await CollectionsWrite.parse(incomingMessage);
-    } catch (e) {
-      return new MessageReply({
-        status: { code: 400, detail: e.message }
-      });
-    }
-
-    // authentication & authorization
-    try {
-      await authenticate(message.authorization, didResolver);
-      await collectionsWrite.authorize(messageStore);
-    } catch (e) {
-      return new MessageReply({
-        status: { code: 401, detail: e.message }
-      });
-    }
-
-    // get existing records matching the `recordId`
-    const query = {
-      target   : collectionsWrite.target,
-      method   : DwnMethodName.CollectionsWrite,
-      recordId : incomingMessage.recordId
-    };
-    const existingMessages = await messageStore.query(query) as CollectionsWriteMessage[];
-    const newestExistingMessage = await CollectionsWrite.getNewestMessage(existingMessages);
-
-    // find which message is the newest, and if the incoming message is the newest
-    let incomingMessageIsNewest = false;
-    let newestMessage;
-    // if incoming message is newest
-    if (newestExistingMessage === undefined || await CollectionsWrite.isNewer(incomingMessage, newestExistingMessage)) {
-      // expected lineage parent of the incoming message should not be specified (ie. an originating message) if no existing record exists
-      // else the expected lineage parent should just point to originating message (logic will change when CollectionsDelete is implemented)
-      const expectedLineageParent = newestExistingMessage?.recordId;
-      const incomingMessageLineageParent = incomingMessage.descriptor.lineageParent;
-      if (incomingMessageLineageParent !== expectedLineageParent) {
-        return new MessageReply({
-          status: { code: 400, detail: `expecting lineageParent to be ${expectedLineageParent} but got ${incomingMessageLineageParent}` }
-        });
-      }
-
-      incomingMessageIsNewest = true;
-      newestMessage = incomingMessage;
-    } else { // existing message is the same age or newer than the incoming message
-      newestMessage = newestExistingMessage;
-    }
-
-    // write the incoming message to DB if incoming message is newest
-    let messageReply: MessageReply;
-    if (incomingMessageIsNewest) {
-      const isLatestBaseState = true;
-      const indexes = constructIndexes(collectionsWrite, isLatestBaseState);
-
-      await messageStore.put(incomingMessage, indexes);
-
-      messageReply = new MessageReply({
-        status: { code: 202, detail: 'Accepted' }
-      });
-    } else {
-      messageReply = new MessageReply({
-        status: { code: 409, detail: 'Conflict' }
-      });
-    }
-
-    // delete all existing messages that are not newest, except for the originating record
-    // NOTE: under normal operation, there should only be at most two existing messages per `recordId`
-    // and at most only one message needs to be deleted (prior to CollectionsDelete implementation),
-    // but the DWN may crash before `delete()` is called below, so we use a loop as tactic to clean up lingering data as needed
-    for (const message of existingMessages) {
-      const messageIsOld = await CollectionsWrite.isOlder(message, newestMessage);
-      if (messageIsOld) {
-        // the easiest implementation here is delete each old messages
-        // and re-create it with the right index (isLatestBaseState = 'false') if the message is the originating message,
-        // but there is room for better/more efficient implementation here
-        const cid = await Message.getCid(message);
-        await messageStore.delete(cid);
-
-        // if the message is the originating message
-        // we actually need to keep it BUT, need to ensure the message is no longer marked as the latest state
-        if (message.descriptor.lineageParent === undefined) {
-          const existingCollectionsWrite = await CollectionsWrite.parse(message);
-          const isLatestBaseState = false;
-          const indexes = constructIndexes(existingCollectionsWrite, isLatestBaseState);
-          await messageStore.put(message, indexes);
-        }
-      }
-    }
-
-    return messageReply;
+    collectionsWrite = await CollectionsWrite.parse(incomingMessage);
   } catch (e) {
     return new MessageReply({
-      status: { code: 500, detail: e.message }
+      status: { code: 400, detail: e.message }
     });
   }
+
+  // authentication & authorization
+  try {
+    await authenticate(message.authorization, didResolver);
+    await collectionsWrite.authorize(messageStore);
+  } catch (e) {
+    return new MessageReply({
+      status: { code: 401, detail: e.message }
+    });
+  }
+
+  // get existing records matching the `recordId`
+  const query = {
+    target   : collectionsWrite.target,
+    method   : DwnMethodName.CollectionsWrite,
+    recordId : incomingMessage.recordId
+  };
+  const existingMessages = await messageStore.query(query) as CollectionsWriteMessage[];
+  const newestExistingMessage = await CollectionsWrite.getNewestMessage(existingMessages);
+
+  // find which message is the newest, and if the incoming message is the newest
+  let incomingMessageIsNewest = false;
+  let newestMessage;
+  // if incoming message is newest
+  if (newestExistingMessage === undefined || await CollectionsWrite.isNewer(incomingMessage, newestExistingMessage)) {
+    // expected lineage parent of the incoming message should not be specified (ie. an originating message) if no existing record exists
+    // else the expected lineage parent should just point to originating message (logic will change when CollectionsDelete is implemented)
+    const expectedLineageParent = newestExistingMessage?.recordId;
+    const incomingMessageLineageParent = incomingMessage.descriptor.lineageParent;
+    if (incomingMessageLineageParent !== expectedLineageParent) {
+      return new MessageReply({
+        status: { code: 400, detail: `expecting lineageParent to be ${expectedLineageParent} but got ${incomingMessageLineageParent}` }
+      });
+    }
+
+    incomingMessageIsNewest = true;
+    newestMessage = incomingMessage;
+  } else { // existing message is the same age or newer than the incoming message
+    newestMessage = newestExistingMessage;
+  }
+
+  // write the incoming message to DB if incoming message is newest
+  let messageReply: MessageReply;
+  if (incomingMessageIsNewest) {
+    const isLatestBaseState = true;
+    const indexes = constructIndexes(collectionsWrite, isLatestBaseState);
+
+    await messageStore.put(incomingMessage, indexes);
+
+    messageReply = new MessageReply({
+      status: { code: 202, detail: 'Accepted' }
+    });
+  } else {
+    messageReply = new MessageReply({
+      status: { code: 409, detail: 'Conflict' }
+    });
+  }
+
+  // delete all existing messages that are not newest, except for the originating record
+  // NOTE: under normal operation, there should only be at most two existing messages per `recordId`
+  // and at most only one message needs to be deleted (prior to CollectionsDelete implementation),
+  // but the DWN may crash before `delete()` is called below, so we use a loop as tactic to clean up lingering data as needed
+  for (const message of existingMessages) {
+    const messageIsOld = await CollectionsWrite.isOlder(message, newestMessage);
+    if (messageIsOld) {
+      // the easiest implementation here is delete each old messages
+      // and re-create it with the right index (isLatestBaseState = 'false') if the message is the originating message,
+      // but there is room for better/more efficient implementation here
+      const cid = await Message.getCid(message);
+      await messageStore.delete(cid);
+
+      // if the message is the originating message
+      // we actually need to keep it BUT, need to ensure the message is no longer marked as the latest state
+      if (message.descriptor.lineageParent === undefined) {
+        const existingCollectionsWrite = await CollectionsWrite.parse(message);
+        const isLatestBaseState = false;
+        const indexes = constructIndexes(existingCollectionsWrite, isLatestBaseState);
+        await messageStore.put(message, indexes);
+      }
+    }
+  }
+
+  return messageReply;
 };
 
 export function constructIndexes(collectionsWrite: CollectionsWrite, isLatestBaseState: boolean): { [key:string]: string } {
--- src/interfaces/protocols/handlers/protocols-configure.ts
+++ src/interfaces/protocols/handlers/protocols-configure.ts
@@ -12,71 +12,65 @@ export const handleProtocolsConfigure: MethodHandler = async (
   messageStore,
   didResolver
 ): Promise<MessageReply> => {
+  const incomingMessage = message as ProtocolsConfigureMessage;
+
+  let protocolsConfigure: ProtocolsConfigure;
   try {
-    const incomingMessage = message as ProtocolsConfigureMessage;
-
-    let protocolsConfigure: ProtocolsConfigure;
-    try {
-      protocolsConfigure = await ProtocolsConfigure.parse(incomingMessage);
-    } catch (e) {
-      return new MessageReply({
-        status: { code: 400, detail: e.message }
-      });
-    }
-
-    // authentication & authorization
-    try {
-      await canonicalAuth(protocolsConfigure, didResolver);
-    } catch (e) {
-      return new MessageReply({
-        status: { code: 401, detail: e.message }
-      });
-    }
-
-    // attempt to get existing protocol
-    const query = {
-      target   : protocolsConfigure.target,
-      method   : DwnMethodName.ProtocolsConfigure,
-      protocol : incomingMessage.descriptor.protocol
-    };
-    const existingMessages = await messageStore.query(query) as ProtocolsConfigureMessage[];
-
-    // find lexicographically the largest message, and if the incoming message is the largest
-    let newestMessage = await Message.getMessageWithLargestCid(existingMessages);
-    let incomingMessageIsNewest = false;
-    if (newestMessage === undefined || await Message.isCidLarger(incomingMessage, newestMessage)) {
-      incomingMessageIsNewest = true;
-      newestMessage = incomingMessage;
-    }
-
-    // write the incoming message to DB if incoming message is largest
-    let messageReply: MessageReply;
-    if (incomingMessageIsNewest) {
-      const { author, target } = protocolsConfigure;
-      const index = { author, target, ... message.descriptor };
-      await messageStore.put(message, index);
-
-      messageReply = new MessageReply({
-        status: { code: 202, detail: 'Accepted' }
-      });
-    } else {
-      messageReply = new MessageReply({
-        status: { code: 409, detail: 'Conflict' }
-      });
-    }
-
-    // delete all existing records that are smaller
-    for (const message of existingMessages) {
-      if (await Message.isCidLarger(newestMessage, message)) {
-        const cid = await Message.getCid(message);
-        await messageStore.delete(cid);
-      }
-    }
-
-    return messageReply;
+    protocolsConfigure = await ProtocolsConfigure.parse(incomingMessage);
   } catch (e) {
     return new MessageReply({
-      status: { code: 500, detail: e.message }
+      status: { code: 400, detail: e.message }
     });
   }
+
+  // authentication & authorization
+  try {
+    await canonicalAuth(protocolsConfigure, didResolver);
+  } catch (e) {
+    return new MessageReply({
+      status: { code: 401, detail: e.message }
+    });
+  }
+
+  // attempt to get existing protocol
+  const query = {
+    target   : protocolsConfigure.target,
+    method   : DwnMethodName.ProtocolsConfigure,
+    protocol : incomingMessage.descriptor.protocol
+  };
+  const existingMessages = await messageStore.query(query) as ProtocolsConfigureMessage[];
+
+  // find lexicographically the largest message, and if the incoming message is the largest
+  let newestMessage = await Message.getMessageWithLargestCid(existingMessages);
+  let incomingMessageIsNewest = false;
+  if (newestMessage === undefined || await Message.isCidLarger(incomingMessage, newestMessage)) {
+    incomingMessageIsNewest = true;
+    newestMessage = incomingMessage;
+  }
+
+  // write the incoming message to DB if incoming message is largest
+  let messageReply: MessageReply;
+  if (incomingMessageIsNewest) {
+    const { author, target } = protocolsConfigure;
+    const index = { author, target, ... message.descriptor };
+    await messageStore.put(message, index);
+
+    messageReply = new MessageReply({
+      status: { code: 202, detail: 'Accepted' }
+    });
+  } else {
+    messageReply = new MessageReply({
+      status: { code: 409, detail: 'Conflict' }
+    });
+  }
+
+  // delete all existing records that are smaller
+  for (const message of existingMessages) {
+    if (await Message.isCidLarger(newestMessage, message)) {
+      const cid = await Message.getCid(message);
+      await messageStore.delete(cid);
+    }
+  }
+
+  return messageReply;
 };
