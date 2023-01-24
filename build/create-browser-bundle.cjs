ed0dd163a0b1399459bcbf93931f41c6330b583b
Moe Jangda <moe@tbd.email>
2023-01-24 01:11:36 -0600

    output bundle metadata

--- build/create-browser-bundle.cjs
+++ build/create-browser-bundle.cjs
@@ -1,6 +1,11 @@
 const browserConfig = require('./esbuild-browser-config.cjs');
+const fs = require('fs');
 
 require('esbuild').build({
   ...browserConfig,
-  outfile: 'dist/bundles/browser.js',
+  metafile : true,
+  outfile  : 'dist/bundles/browser.js',
+}).then(result => {
+  const serializedMetafile = JSON.stringify(result.metafile, null, 4);
+  fs.writeFileSync(`${__dirname}/../bundle-metadata.json`, serializedMetafile, { encoding: 'utf8' });
 });
