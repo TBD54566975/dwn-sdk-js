a59d5192dcd81a356df0e743f0dd95daaf2a1385
Moe Jangda <moe@tbd.email>
2023-01-25 22:16:35 -0600

    Browser Bundle Improvements (#200)
    * bump `ed25519` and `secp256k1` libs
    * 0.0.18
    * import specific `lodash` method to reduce bundle size
    * output bundle metadata
    * improve browser bundle
    * include safari and firefox as targets
    * run headless browser tests in safari and firefox
    * use non-abandoned polyfill lib
    * add cjs dist and include  in
    * patch security vuln
    * include `package.json` in `dist/cjs` to override `type: module` in parent `package.json`
    * add react-native directive to package.json

--- build/esbuild-browser-config.cjs
+++ build/esbuild-browser-config.cjs
@@ -1,13 +1,17 @@
-const { NodeGlobalsPolyfillPlugin } = require('@esbuild-plugins/node-globals-polyfill');
+const polyfillProviderPlugin = require('node-stdlib-browser/helpers/esbuild/plugin');
+const stdLibBrowser = require('node-stdlib-browser');
 
+/** @type {import('esbuild').BuildOptions} */
 module.exports = {
   entryPoints : ['./src/index.ts'],
   bundle      : true,
   format      : 'esm',
   sourcemap   : true,
+  minify      : true,
   platform    : 'browser',
-  target      : ['chrome101'],
-  plugins     : [NodeGlobalsPolyfillPlugin()],
+  target      : ['chrome101', 'firefox108', 'safari16'],
+  inject      : [require.resolve('node-stdlib-browser/helpers/esbuild/shim')],
+  plugins     : [polyfillProviderPlugin(stdLibBrowser)],
   define      : {
     'global': 'globalThis'
   }
