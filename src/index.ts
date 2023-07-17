ea795e28bbda63110d3a70ec499ef9e5c56c41b2
Adam Mika <amika@squareup.com>
2023-07-17 11:30:18 -0600

    Add exports for DataStore, MessageStore, and EventLog types

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 // export everything that we want to be consumable
 export type { DwnConfig } from './dwn.js';
 export type { DwnServiceEndpoint, ServiceEndpoint, DidDocument, DidResolutionResult, DidResolutionMetadata, DidDocumentMetadata, VerificationMethod } from './did/did-resolver.js';
-export type { EventLog, Event } from './types/event-log.js';
+export type { EventLog, Event, GetEventsOptions } from './types/event-log.js';
 export type { EventsGetMessage, EventsGetReply } from './types/event-types.js';
 export type { HooksWriteMessage } from './types/hooks-types.js';
-export type { GenericMessage } from './types/message-types.js';
+export type { GenericMessage, Filter } from './types/message-types.js';
 export type { MessagesGetMessage, MessagesGetReply } from './types/messages-types.js';
 export type { PermissionConditions, PermissionScope, PermissionsGrantDescriptor, PermissionsGrantMessage, PermissionsRequestDescriptor, PermissionsRequestMessage, PermissionsRevokeDescriptor, PermissionsRevokeMessage } from './types/permissions-types.js';
 export type { ProtocolsConfigureDescriptor, ProtocolDefinition, ProtocolRuleSet, ProtocolsQueryFilter, ProtocolsConfigureMessage, ProtocolsQueryMessage } from './types/protocols-types.js';
@@ -12,7 +12,7 @@ export type { EncryptionProperty, RecordsDeleteMessage, RecordsQueryMessage, Rec
 export type { SnapshotsCreateDescriptor, SnapshotsCreateMessage, SnapshotDefinition, SnapshotScope, SnapshotScopeType } from './types/snapshots-types.js';
 export { AllowAllTenantGate, TenantGate } from './core/tenant-gate.js';
 export { Cid } from './utils/cid.js';
-export { DataStore } from './types/data-store.js';
+export { DataStore, PutResult, GetResult, AssociateResult } from './types/data-store.js';
 export { DataStoreLevel } from './store/data-store-level.js';
 export { DateSort } from './interfaces/records-query.js';
 export { DataStream } from './utils/data-stream.js';
@@ -49,3 +49,4 @@ export { RecordsRead, RecordsReadOptions } from './interfaces/records-read.js';
 export { SnapshotsCreate, SnapshotsCreateOptions } from './interfaces/snapshots-create.js';
 export { Secp256k1 } from './utils/secp256k1.js';
 export { SignatureInput } from './types/jws-types.js';
+export { executeUnlessAborted } from './utils/abort.js';
