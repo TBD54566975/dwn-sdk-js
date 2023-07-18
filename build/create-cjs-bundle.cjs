5eaa5e327c291918d4da92df5adea88e0d720581
Moe Jangda <moe@tbd.email>
2023-07-17 18:50:53 -0700

    change bundling strategy

---
+++ build/create-cjs-bundle.cjs
@@ -0,0 +1,48 @@
+const esbuild = require('esbuild');
+const packageJson = require('../package.json');
+
+// list of dependencies that _dont_ ship cjs
+const includeList = new Set([
+  '@ipld/dag-cbor',
+  '@noble/ed25519',
+  '@noble/secp256k1',
+  'blockstore-core',
+  'ipfs-unixfs-exporter',
+  'ipfs-unixfs-importer',
+  'multiformats'
+]);
+
+// create list of dependencies that we _do not_ want to include in our bundle
+const excludeList = [];
+for (const dependency in packageJson.dependencies) {
+  if (includeList.has(dependency)) {
+    continue;
+  } else {
+    excludeList.push(dependency);
+  }
+}
+
+/** @type {import('esbuild').BuildOptions} */
+const baseConfig = {
+  platform : 'node',
+  format   : 'cjs',
+  bundle   : true,
+  external : excludeList,
+};
+
+const indexConfig = {
+  ...baseConfig,
+  entryPoints : ['./dist/esm/src/index.js'],
+  outfile     : './dist/cjs/index.js',
+};
+
+esbuild.buildSync(indexConfig);
+
+
+const storesConfig = {
+  ...baseConfig,
+  entryPoints : ['./dist/esm/src/index-stores.js'],
+  outfile     : './dist/cjs/index-stores.js',
+};
+
+esbuild.buildSync(storesConfig);
