05fcb8a0d0e3da582df9d0d54ad60b65b6cb4da9
nearlyjuly <112702586+nearlyjuly@users.noreply.github.com>
2023-08-16 15:09:42 +0100

    Update tenant-gate.ts
    Adding a custom tenantgate option to support any tenant list checking function that returns a boolean value

--- src/core/tenant-gate.ts
+++ src/core/tenant-gate.ts
@@ -16,3 +16,14 @@ export class AllowAllTenantGate implements TenantGate {
     return true;
   }
 }
+/**
+ * A tenant gate that returns the value of externalTenantCheck, which requires a custom tenant checking function.
+ * The default response is 'false'.
+ */
+export class ExternalTenantGate implements TenantGate {
+  constructor(private externalTenantCheck: Function) {}
+  public async isTenant(did: string): Promise<boolean> {
+    const tenantCheck = await this.externalTenantCheck(did);
+    return tenantCheck;
+  }
+}
