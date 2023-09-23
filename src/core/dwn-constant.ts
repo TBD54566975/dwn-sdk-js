1731caacf9b0c82fe765505891efc53627234a06
Liran Cohen <c.liran.c@gmail.com>
2023-09-22 21:34:58 -0400

    update maxDataSizeAllowedToBeEncoded to a non arbitrary round number, and updated comment to explain the limit

--- src/core/dwn-constant.ts
+++ src/core/dwn-constant.ts
@@ -1,7 +1,9 @@
 export class DwnConstant {
   /**
-   * The maximum size in bytes of raw data that will be returned as `encodedData`.
-   * this is also the maximum size that we will store within MessageStore.
+   * The maximum size of raw data that will be returned as `encodedData`.
+   *
+   * We chose 30k, as after encoding it would give plenty of headroom up to the 65k limit in most SQL variants.
+   * We currently encode using base64url which is a 33% increase in size.
    */
-  public static readonly maxDataSizeAllowedToBeEncoded = 42_000;
+  public static readonly maxDataSizeAllowedToBeEncoded = 30_000;
 }
