c3e85537ea7894a8eb68b7d175f71b0a09a90f7e
Andor Kesselman <andor@henosisknot.com>
2023-09-27 15:30:57 +0530

    added events create interface

---
+++ src/interfaces/event-create.ts
@@ -0,0 +1,35 @@
+import { getCurrentTimeInHighPrecision } from '../utils/time.js';
+import { removeUndefinedProperties } from '../utils/object.js';
+import type { Signer } from '../index.js';
+
+import { DwnInterfaceName, DwnMethodName, Message } from '../core/message.js';
+import type { EventDescriptor, EventMessageI, EventsCreateDescriptor } from '../types/event-types.js';
+
+export type EventCreateOptions = {
+    descriptor: EventDescriptor;
+    messageId?: string;
+    authorizationSignatureInput?: Signer;
+    permissionsGrantId?: string;
+    messageTimestamp?: string;
+};
+
+export class EventMessage extends Message<EventMessageI<any>> {
+
+  static async create(options: EventCreateOptions): Promise<EventMessage> {
+    const descriptor: EventsCreateDescriptor = {
+      interface        : DwnInterfaceName.Subscriptions,
+      method           : DwnMethodName.Request,
+      messageTimestamp : options.messageTimestamp ?? getCurrentTimeInHighPrecision(),
+      messageId        : options.messageId,
+      eventDescriptor  : options.descriptor,
+    };
+    removeUndefinedProperties(descriptor);
+    const authorization = options.authorizationSignatureInput ?
+      await Message.signAsAuthorization(descriptor, options.authorizationSignatureInput) : undefined;
+    const message = { descriptor: descriptor, authorization: authorization };
+    // Message.validateJsonSchema(message);
+    // TODO: @andorsk Fix schema validation....
+    return new EventMessage(message);
+  }
+
+}
