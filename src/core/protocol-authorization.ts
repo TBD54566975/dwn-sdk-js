d1b6108b1b5b33bcbbe7ba6f52f1b9449ed3f927
Diane Huxley <dhuxley@tbd.email>
2023-10-03 16:06:31 -0700

    Remote outdated comment

--- src/core/protocol-authorization.ts
+++ src/core/protocol-authorization.ts
@@ -341,7 +341,6 @@ export class ProtocolAuthorization {
       throw new Error(`no action rule defined for ${incomingMessage.message.descriptor.method}, ${author} is unauthorized`);
     }
 
-    // Get role being invoked. Currently only Reads support role-based authorization
     const invokedRole = incomingMessage.authorizationPayload?.protocolRole;
 
     for (const actionRule of actionRules) {
