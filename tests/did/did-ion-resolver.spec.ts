754135962ca384ec75bc38eed66101b3b1a3998d
Liran Cohen <c.liran.c@gmail.com>
2023-10-05 17:10:23 -0400

    revert network unavailable stub, breaks in the browser test

--- tests/did/did-ion-resolver.spec.ts
+++ tests/did/did-ion-resolver.spec.ts
@@ -1,6 +1,4 @@
 import chaiAsPromised from 'chai-as-promised';
-import fetch from 'cross-fetch';
-import sinon from 'sinon';
 import chai, { expect } from 'chai';
 
 import { DidIonResolver } from '../../src/did/did-ion-resolver.js';
@@ -10,21 +8,6 @@ chai.use(chaiAsPromised);
 
 describe('DidIonResolver', () => {
   const defaultResolutionEndpoint = 'https://discover.did.msidentity.com/1.0/identifiers/';
-  let networkAvailable = false;
-  before(async () => {
-    // test network connectivity, `networkAvailable` is used by tests to decide whether to run tests through real network calls or stubs
-    const testDidUrl = `${defaultResolutionEndpoint}did:ion:EiClkZMDxPKqC9c-umQfTkR8vvZ9JPhl_xLDI9Nfk38w5w`;
-
-    try {
-      const response = await fetch(testDidUrl);
-
-      if (response.status === 200) {
-        networkAvailable = true;
-      }
-    } catch {
-      // no op, all tests will run through stubs
-    }
-  });
 
   it('should set a default resolution endpoint when none is given in constructor', async () => {
     const didIonResolver = new DidIonResolver();
@@ -36,22 +19,7 @@ describe('DidIonResolver', () => {
     const did = 'did:ion:EiClkZMDxPKqC9c-umQfTkR8vvZ9JPhl_xLDI9Nfk38w5w';
     const didIonResolver = new DidIonResolver();
 
-    let resolverStub;
-    // stub network call if network is not available
-    if (!networkAvailable) {
-      resolverStub = sinon.stub(didIonResolver as any, 'fetch').resolves({
-        status : 200,
-        json   : async () => Promise.resolve({
-          didDocument         : { id: did },
-          didDocumentMetadata : { canonicalId: did }
-        })
-      });
-    }
-
     const resolutionDocument = await didIonResolver.resolve(did);
-    if (resolverStub) {
-      resolverStub.restore();
-    }
     expect(resolutionDocument.didDocument?.id).to.equal(did);
     expect(resolutionDocument.didDocumentMetadata.canonicalId).to.equal(did);
   });
@@ -60,16 +28,7 @@ describe('DidIonResolver', () => {
     const did = 'did:ion:SomethingThatCannotBeResolved';
     const didIonResolver = new DidIonResolver();
 
-    let resolverStub;
-    // stub network call if network is not available
-    if (!networkAvailable) {
-      resolverStub = sinon.stub(didIonResolver as any, 'fetch').resolves({ status: 404 });
-    }
-
     const resolutionPromise = didIonResolver.resolve(did);
-    if (resolverStub) {
-      resolverStub.restore();
-    }
     await expect(resolutionPromise).to.be.rejectedWith('unable to resolve');
   });
 });
