1529f8bdcd59af5433ebffbfc2240cce3bbcf597
Liran Cohen <c.liran.c@gmail.com>
2023-10-05 21:45:48 -0400

    refactor cleanup

--- src/store/message-store-level.ts
+++ src/store/message-store-level.ts
@@ -13,7 +13,7 @@ import { CID } from 'multiformats/cid';
 import { createLevelDatabase } from './level-wrapper.js';
 import { executeUnlessAborted } from '../utils/abort.js';
 import { Message } from '../core/message.js';
-import { MessageIndex } from './index-level.js';
+import { MessageIndex } from '../store/message-index.js';
 import { sha256 } from 'multiformats/hashes/sha2';
 import { SortOrder } from '../types/message-types.js';
 
