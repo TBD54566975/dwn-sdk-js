89f0bd96c2517c266d59833a4e59505d9177cf52
Shobit Beltangdy <shobitb@gmail.com>
2023-10-15 22:36:46 -0700

    Add tests for error cases in did-resolver.spec.ts
    This commit adds new tests in did-resolver.spec.ts to cover some of the previously uncovered error-related branching paths.
    
    This commit contributes partly to issue #136.
    
    Based on the output of `npm run test:node` before vs. after this commit, the `% Branch` is now increased by 9.66 percentage points, and the `% Stmt` and `% Lines` by 3.29 percentage points.
    
    Before:
    ```
    did-resolver.ts |   87.91 |    78.57 |   66.66 |   87.91
    ```
    
    After:
    ```
    did-resolver.ts |    91.2 |    88.23 |   66.66 |    91.2 |
    ```

--- tests/did/did-resolver.spec.ts
+++ tests/did/did-resolver.spec.ts
@@ -32,4 +32,63 @@ describe('DidResolver', () => {
     sinon.assert.calledTwice(cacheGetSpy); // should try to fetch from cache both times
     sinon.assert.calledOnce(ionDidResolveSpy); // should only resolve using ION resolver once (the first time)
   });
+
+  it('should throw error when invalid DID is used', async () => {
+    const did = 'did:ion:invalidDid';
+    const didIonResolver = new DidIonResolver();
+    const didResolver = new DidResolver([didIonResolver]);
+
+    await expect(didResolver.resolve(did)).to.be.rejectedWith(Error);
+  });
+
+  it('should throw error when unsupported DID method is used', async () => {
+    const did = 'did:unsupportedDidMethod:EiClkZMDxPKqC9c-umQfTkR8vvZ9JPhl_xLDI9Nfk38w5w';
+    const didIonResolver = new DidIonResolver();
+    const didResolver = new DidResolver([didIonResolver]);
+
+    await expect(didResolver.resolve(did)).to.be.rejectedWith(Error);
+  });
+
+  it('should throw error when resolution fails due to error in didResolutionMetadata', async () => {
+    const did = 'did:ion:EiClkZMDxPKqC9c-umQfTkR8vvZ9JPhl_xLDI9Nfk38w5w';
+    const didIonResolver = new DidIonResolver('unusedResolutionEndpoint');
+    const didResolver = new DidResolver([didIonResolver]);
+
+    const mockResolution = {
+      didDocument           : 'any' as any,
+      didResolutionMetadata : { error: 'some error' },
+      didDocumentMetadata   : 'any' as any
+    };
+
+    const ionDidResolveSpy = sinon.stub(didIonResolver, 'resolve').resolves(mockResolution);
+    const cacheGetSpy = sinon.spy(didResolver['cache'], 'get');
+
+    await expect(didResolver.resolve(did)).to.be.rejectedWith(Error);
+
+    sinon.assert.calledOnce(cacheGetSpy);
+    sinon.assert.calledOnce(ionDidResolveSpy);
+
+  });
+
+  it('should throw error when resolution fails due to undefined didDocument', async () => {
+    const did = 'did:ion:EiClkZMDxPKqC9c-umQfTkR8vvZ9JPhl_xLDI9Nfk38w5w';
+    const didIonResolver = new DidIonResolver('unusedResolutionEndpoint');
+    const didResolver = new DidResolver([didIonResolver]);
+
+    const mockResolution = {
+      didDocument           : undefined,
+      didResolutionMetadata : 'any' as any,
+      didDocumentMetadata   : 'any' as any
+    };
+
+    const ionDidResolveSpy = sinon.stub(didIonResolver, 'resolve').resolves(mockResolution);
+    const cacheGetSpy = sinon.spy(didResolver['cache'], 'get');
+
+    await expect(didResolver.resolve(did)).to.be.rejectedWith(Error);
+
+    sinon.assert.calledOnce(cacheGetSpy);
+    sinon.assert.calledOnce(ionDidResolveSpy);
+
+  });
+
 });
