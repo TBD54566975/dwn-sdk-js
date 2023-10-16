36082ce2230a32422d295c7bf79a929efcade892
Shobit Beltangdy <shobitb@gmail.com>
2023-10-15 22:36:46 -0700

    Add tests for error cases in did-resolver.spec.ts
    This commit adds new tests in did-resolver.spec.ts to cover some of the previously uncovered error-related branching paths.
    
    This commit contributes partly to issue #136.
    
    Based on the output of `npm run test:node` before vs. after this commit, the `% Branch` is now increased by 9.66 percentage points, and the `% Stmt` and `% Lines` by 3.29 percentage points.
    
    Before:
    ```
    did-resolver.ts |   87.91 |    78.57 |   66.66 |   87.91
    ```
    
    After:
    ```
    did-resolver.ts |   92.34 |    94.11 |   66.66 |   92.34 |
    ```

--- tests/did/did-resolver.spec.ts
+++ tests/did/did-resolver.spec.ts
@@ -4,6 +4,7 @@ import chai, { expect } from 'chai';
 
 import { DidIonResolver } from '../../src/did/did-ion-resolver.js';
 import { DidResolver } from '../../src/did/did-resolver.js';
+import { DwnErrorCode } from '../../src/core/dwn-error.js';
 
 // extends chai to test promises
 chai.use(chaiAsPromised);
@@ -34,19 +35,19 @@ describe('DidResolver', () => {
   });
 
   it('should throw error when invalid DID is used', async () => {
-    const did = 'did:ion:invalidDid';
+    const did = 'blah';
     const didIonResolver = new DidIonResolver();
     const didResolver = new DidResolver([didIonResolver]);
 
-    await expect(didResolver.resolve(did)).to.be.rejectedWith(Error);
+    await expect(didResolver.resolve(did)).to.eventually.be.rejected.and.has.property('code', DwnErrorCode.DidNotValid);
   });
 
   it('should throw error when unsupported DID method is used', async () => {
-    const did = 'did:unsupportedDidMethod:EiClkZMDxPKqC9c-umQfTkR8vvZ9JPhl_xLDI9Nfk38w5w';
+    const did = 'did:foo:EiClkZMDxPKqC9c-umQfTkR8vvZ9JPhl_xLDI9Nfk38w5w';
     const didIonResolver = new DidIonResolver();
     const didResolver = new DidResolver([didIonResolver]);
 
-    await expect(didResolver.resolve(did)).to.be.rejectedWith(Error);
+    await expect(didResolver.resolve(did)).to.eventually.be.rejected.and.has.property('code', DwnErrorCode.DidMethodNotSupported);
   });
 
   it('should throw error when resolution fails due to error in didResolutionMetadata', async () => {
@@ -63,7 +64,7 @@ describe('DidResolver', () => {
     const ionDidResolveSpy = sinon.stub(didIonResolver, 'resolve').resolves(mockResolution);
     const cacheGetSpy = sinon.spy(didResolver['cache'], 'get');
 
-    await expect(didResolver.resolve(did)).to.be.rejectedWith(Error);
+    await expect(didResolver.resolve(did)).to.eventually.be.rejected.and.has.property('code', DwnErrorCode.DidResolutionFailed);
 
     sinon.assert.calledOnce(cacheGetSpy);
     sinon.assert.calledOnce(ionDidResolveSpy);
@@ -84,7 +85,7 @@ describe('DidResolver', () => {
     const ionDidResolveSpy = sinon.stub(didIonResolver, 'resolve').resolves(mockResolution);
     const cacheGetSpy = sinon.spy(didResolver['cache'], 'get');
 
-    await expect(didResolver.resolve(did)).to.be.rejectedWith(Error);
+    await expect(didResolver.resolve(did)).to.eventually.be.rejected.and.has.property('code', DwnErrorCode.DidResolutionFailed);
 
     sinon.assert.calledOnce(cacheGetSpy);
     sinon.assert.calledOnce(ionDidResolveSpy);
