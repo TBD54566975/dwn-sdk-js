ee710883ad0776e360aad6d15c7124e79b72183b
Liran Cohen <c.liran.c@gmail.com>
2024-01-11 15:53:27 -0500

    simplify the EventStream interface and logic

--- src/event-log/event-stream.ts
+++ src/event-log/event-stream.ts
@@ -1,38 +1,24 @@
-import type { MessageStore } from '../types/message-store.js';
-import type { EventsSubscribeMessage, EventsSubscription } from '../types/events-types.js';
-import type { EventStream, SubscriptionHandler } from '../types/subscriptions.js';
-import type { Filter, KeyValues } from '../types/query-types.js';
-import type { GenericMessage, GenericMessageSubscription } from '../types/message-types.js';
+import type { GenericMessage } from '../types/message-types.js';
+import type { KeyValues } from '../types/query-types.js';
+import type { EventListener, EventStream, EventSubscription } from '../types/subscriptions.js';
 
 import { EventEmitter } from 'events';
-import { EventsSubscribe } from '../interfaces/events-subscribe.js';
-import { EventsSubscriptionHandler } from '../handlers/events-subscribe.js';
-import { Message } from '../core/message.js';
-import { DwnError, DwnErrorCode } from '../core/dwn-error.js';
 
-const eventChannel = 'events';
+const EVENTS_LISTENER_CHANNEL = 'events';
 
 type EventStreamConfig = {
   emitter?: EventEmitter;
-  reauthorizationTTL?: number;
 };
 
 export class EventStreamEmitter implements EventStream {
   private eventEmitter: EventEmitter;
-  private reauthorizationTTL: number;
 
   private isOpen: boolean = false;
-  private subscriptions: Map<string, SubscriptionHandler> = new Map();
 
   constructor(config?: EventStreamConfig) {
-    this.reauthorizationTTL = config?.reauthorizationTTL || 0; // if set to zero it does not reauthorize
-
     // we capture the rejections and currently just log the errors that are produced
     this.eventEmitter = config?.emitter || new EventEmitter({ captureRejections: true });
-  }
-
-  private get eventChannel(): string {
-    return `${eventChannel}_bus`;
+    this.eventEmitter.on('error', this.eventError);
   }
 
   // we subscribe to the general `EventEmitter` error events with this handler.
@@ -41,43 +27,15 @@ export class EventStreamEmitter implements EventStream {
     console.error('event emitter error', error);
   };
 
-  async subscribe(tenant: string, message: EventsSubscribeMessage, filters: Filter[], messageStore: MessageStore): Promise<EventsSubscription>;
-  async subscribe(tenant: string, message: GenericMessage, filters: Filter[], messageStore: MessageStore): Promise<GenericMessageSubscription> {
-    const messageCid = await Message.getCid(message);
-    let subscription = this.subscriptions.get(messageCid);
-    if (subscription !== undefined) {
-      return subscription;
-    }
-
-    if (EventsSubscribe.isEventsSubscribeMessage(message)) {
-      subscription = await EventsSubscriptionHandler.create({
-        tenant,
-        message,
-        filters,
-        messageStore,
-        unsubscribe  : () => this.unsubscribe(messageCid),
-        eventEmitter : this.eventEmitter,
-      });
-    } else {
-      throw new DwnError(DwnErrorCode.EventStreamSubscriptionNotSupported, 'not a supported subscription message');
-    }
-
-    this.subscriptions.set(messageCid, subscription);
-    this.eventEmitter.addListener(this.eventChannel, subscription.listener);
-
-    return subscription;
-  }
-
-  private async unsubscribe(id:string): Promise<void> {
-    const subscription = this.subscriptions.get(id);
-    if (subscription !== undefined) {
-      this.subscriptions.delete(id);
-      this.eventEmitter.removeListener(this.eventChannel, subscription.listener);
-    }
+  async subscribe(id: string, listener: EventListener): Promise<EventSubscription> {
+    this.eventEmitter.on(EVENTS_LISTENER_CHANNEL, listener);
+    return {
+      id,
+      close: async (): Promise<void> => { this.eventEmitter.off(EVENTS_LISTENER_CHANNEL, listener); }
+    };
   }
 
   async open(): Promise<void> {
-    this.eventEmitter.on('error', this.eventError);
     this.isOpen = true;
   }
 
@@ -92,7 +50,7 @@ export class EventStreamEmitter implements EventStream {
       return;
     }
     try {
-      this.eventEmitter.emit(this.eventChannel, tenant, message, indexes);
+      this.eventEmitter.emit(EVENTS_LISTENER_CHANNEL, tenant, message, indexes);
     } catch (error) {
       this.eventError(error);
     }
--- src/event-log/subscription.ts
+++ src/event-log/subscription.ts
@@ -1,7 +1,6 @@
 import type { DwnError } from '../core/dwn-error.js';
 import type { EventEmitter } from 'events';
-import type { MessageStore } from '../types/message-store.js';
-import type { EmitFunction, SubscriptionHandler } from '../types/subscriptions.js';
+import type { EventListener } from '../types/subscriptions.js';
 import type { Filter, KeyValues } from '../types/query-types.js';
 import type { GenericMessage, GenericMessageHandler } from '../types/message-types.js';
 
@@ -10,14 +9,12 @@ import { FilterUtility } from '../utils/filter.js';
 /**
  * Base class to extend default subscription behavior.
  */
-export class SubscriptionHandlerBase implements SubscriptionHandler {
+export class SubscriptionHandlerBase {
   protected eventEmitter: EventEmitter;
-  protected messageStore: MessageStore;
   protected filters: Filter[];
   protected tenant: string;
   protected message: GenericMessage;
 
-  #unsubscribe: () => Promise<void>;
   #id: string;
 
   protected constructor(options: {
@@ -26,19 +23,15 @@ export class SubscriptionHandlerBase implements SubscriptionHandler {
       id: string,
       filters: Filter[],
       eventEmitter: EventEmitter,
-      messageStore: MessageStore,
-      unsubscribe: () => Promise<void>;
     }
   ) {
-    const { tenant, id, filters, eventEmitter, message, messageStore, unsubscribe } = options;
+    const { tenant, id, filters, eventEmitter, message } = options;
 
     this.tenant = tenant;
     this.#id = id;
     this.filters = filters;
     this.eventEmitter = eventEmitter;
     this.message = message;
-    this.messageStore = messageStore;
-    this.#unsubscribe = unsubscribe;
   }
 
   get eventChannel(): string {
@@ -57,7 +50,7 @@ export class SubscriptionHandlerBase implements SubscriptionHandler {
     return tenant === this.tenant && FilterUtility.matchAnyFilter(indexes, this.filters);
   }
 
-  public listener: EmitFunction = (tenant, message, indexes):void => {
+  public listener: EventListener = (tenant, message, indexes):void => {
     if (this.matchFilters(tenant, indexes)) {
       this.eventEmitter.emit(this.eventChannel, message);
     }
@@ -79,6 +72,6 @@ export class SubscriptionHandlerBase implements SubscriptionHandler {
   async close(): Promise<void> {
     this.eventEmitter.removeAllListeners(this.eventChannel);
     this.eventEmitter.removeAllListeners(this.errorEventChannel);
-    await this.#unsubscribe();
+    // await this.#unsubscribe();
   }
 }
