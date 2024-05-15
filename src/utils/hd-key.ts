0c96d2d5207d748168f6b0491b73552a27969f32
Henry Tsai <henrytsai@outlook.com>
2024-05-15 11:41:55 -0700

    Removed the need for passing undefined salt

--- src/utils/hd-key.ts
+++ src/utils/hd-key.ts
@@ -72,7 +72,6 @@ export class HdKey {
       currentPrivateKey = await HdKey.deriveKeyUsingHkdf({
         hashAlgorithm      : 'SHA-256',
         initialKeyMaterial : currentPrivateKey,
-        salt               : undefined,
         info               : segmentBytes, // use the segment as the application specific info for key derivation
         keyLengthInBytes   : 32 // 32 bytes = 256 bits
       });
@@ -88,12 +87,10 @@ export class HdKey {
   public static async deriveKeyUsingHkdf(params: {
     hashAlgorithm: 'SHA-256' | 'SHA-384' | 'SHA-512',
     initialKeyMaterial: Uint8Array,
-    salt: Uint8Array | undefined,
     info: Uint8Array,
     keyLengthInBytes: number
   }): Promise<Uint8Array> {
-    const { hashAlgorithm, initialKeyMaterial, salt, info, keyLengthInBytes } = params;
-    const finalSalt = salt ?? new Uint8Array(0); // if salt is not provided, use an empty array
+    const { hashAlgorithm, initialKeyMaterial, info, keyLengthInBytes } = params;
 
     const webCrypto = getWebcryptoSubtle() as SubtleCrypto;
 
@@ -102,7 +99,12 @@ export class HdKey {
 
     // Derive the bytes using the Web Crypto API.
     const derivedKeyBuffer = await crypto.subtle.deriveBits(
-      { name: 'HKDF', hash: hashAlgorithm, salt: finalSalt, info },
+      {
+        name : 'HKDF',
+        hash : hashAlgorithm,
+        salt : new Uint8Array(0), // `info` should be sufficient in our use case
+        info
+      },
       webCryptoKey,
       keyLengthInBytes * 8 // convert from bytes to bits
     );
