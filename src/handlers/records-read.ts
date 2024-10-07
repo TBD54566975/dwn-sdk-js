8ea1f0afb1f4c65e5ae2b3071e8022bfe6fe99ca
Henry Tsai <henrytsai@outlook.com>
2024-10-07 10:41:02 -0700

    Added TODO

--- src/handlers/records-read.ts
+++ src/handlers/records-read.ts
@@ -65,9 +65,9 @@ export class RecordsReadHandler implements MethodHandler {
 
     const matchedMessage = existingMessages[0];
 
-    // if the matched message is a RecordsDelete, we mark the record as not-found and return both the RecordsDelete the initial write
-    // NOTE: there is an opportunity in the future here to restrict this behavior further:
-    //       ie. perform similar authorization checks as when records exists before returning data about deleted record
+    // if the matched message is a RecordsDelete, we mark the record as not-found and return both the RecordsDelete and the initial RecordsWrite
+    // TODO: https://github.com/TBD54566975/dwn-sdk-js/issues/819:
+    // Consider performing authorization checks like when records exists before returning RecordsDelete and initial RecordsWrite of a deleted record
     if (matchedMessage.descriptor.method === DwnMethodName.Delete) {
       const recordsDeleteMessage = matchedMessage as RecordsDeleteMessage;
       const initialWrite = await RecordsWrite.fetchInitialRecordsWriteMessage(this.messageStore, tenant, recordsDeleteMessage.descriptor.recordId);
