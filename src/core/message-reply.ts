10d863ed57d630e0d11649b61d63ed3ac9397986
Henry Tsai <henrytsai@outlook.com>
2024-10-07 15:29:00 -0700

    Corrected UnionMessageReply type for downstream consumption

--- src/core/message-reply.ts
+++ src/core/message-reply.ts
@@ -1,8 +1,7 @@
 import type { MessagesReadReplyEntry } from '../types/messages-types.js';
 import type { PaginationCursor } from '../types/query-types.js';
 import type { ProtocolsConfigureMessage } from '../types/protocols-types.js';
-import type { Readable } from 'readable-stream';
-import type { RecordsWriteMessage } from '../types/records-types.js';
+import type { RecordsReadReply } from '../types/records-types.js';
 import type { GenericMessageReply, MessageSubscription, QueryResultEntry } from '../types/message-types.js';
 
 export function messageReplyFromError(e: unknown, code: number): GenericMessageReply {
@@ -15,7 +14,7 @@ export function messageReplyFromError(e: unknown, code: number): GenericMessageR
 /**
  * Catch-all message reply type. It is recommended to use GenericMessageReply or a message-specific reply type wherever possible.
  */
-export type UnionMessageReply = GenericMessageReply & {
+export type UnionMessageReply = GenericMessageReply & RecordsReadReply &{
   /**
    * Resulting message entries or events returned from the invocation of the corresponding message.
    * e.g. the resulting messages from a RecordsQuery, or array of messageCid strings for MessagesQuery
@@ -28,18 +27,6 @@ export type UnionMessageReply = GenericMessageReply & {
    * Mutually exclusive with `record`, `entries` and `cursor`.
    */
   entry?: MessagesReadReplyEntry;
-
-  /**
-   * Record corresponding to the message received if applicable (e.g. RecordsRead).
-   * Mutually exclusive with `entries` and `cursor`.
-   */
-  record?: RecordsWriteMessage & {
-    /**
-     * The initial write of the record if the returned RecordsWrite message itself is not the initial write.
-     */
-    initialWrite?: RecordsWriteMessage;
-    data: Readable;
-  };
 
   /**
    * A cursor for pagination if applicable (e.g. RecordsQuery).
